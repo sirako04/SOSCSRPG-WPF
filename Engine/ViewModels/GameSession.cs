bab6856b75ec5a8c8184d0d1a66ca3b986024a60
Sirako <karissirak6@gmail.com>
2024-12-02 22:39:11 +0100

    Add project files.

---
+++ Engine/ViewModels/GameSession.cs
@@ -0,0 +1,19 @@
+﻿using Engine.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.ViewModels
+{
+    public class GameSession
+    {
+        Player CurrentPlayer { get; set; }
+        public GameSession() 
+        {
+            Player CurrentPlayer = new Player();
+            CurrentPlayer.Name = "Sirak";
+        }
+    }
+}

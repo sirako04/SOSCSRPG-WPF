b3965b6f9303ac1db510a1cebec9e8ddfd25569c
Sirako <karissirak6@gmail.com>
2024-12-07 17:11:46 +0100

    moving added

--- Engine/ViewModels/GameSession.cs
+++ Engine/ViewModels/GameSession.cs
@@ -4,17 +4,61 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Engine.Factories;
+using System.ComponentModel;
+using System.Configuration.Assemblies;
 
 namespace Engine.ViewModels
 {
-    public class GameSession
+    public class GameSession : INotifyPropertyChanged
     {
+        private Location _currentLocation;
+        public World CurrentWorld { get; set; }
         public Player CurrentPlayer { get; set; }
-        public Location CurrentLocation { get; set; }
+        public Location CurrentLocation 
+        { 
+            get { return _currentLocation; }
+            set 
+            {
+                _currentLocation = value; 
+                OnPropertyChanged(nameof(CurrentLocation));
+            }
+        }
         public GameSession()
         {
             Player CurrentPlayer = new Player("Sirak", "Fighter", 15, 0, 1, 50);
-            CurrentLocation = new Location(0, -1, "Home", "This is your home", "Image");
+            CurrentLocation = new Location(0, -1, "Home", "This is your home",
+                "/Engine;component/Images/Locations/Home.png");
+            WorldFactory factory = new WorldFactory();
+            CurrentWorld = factory.CreateWorld();
+            CurrentLocation = CurrentWorld.locationAt(0, -1);
         }
+        public void MoveNorth()
+        {
+            CurrentLocation = CurrentWorld.locationAt
+            (CurrentLocation.XCoordinate, CurrentLocation.YCoordinate + 1);
+        }
+        public void MoveEast()
+        {
+            CurrentLocation = CurrentWorld.locationAt
+            (CurrentLocation.XCoordinate + 1, CurrentLocation.YCoordinate);
+
+        }
+        public void MoveSouth()
+        {
+            CurrentLocation = CurrentWorld.locationAt
+            (CurrentLocation.XCoordinate, CurrentLocation.YCoordinate - 1);
+        }
+        public void MoveWest()
+        {
+            CurrentLocation = CurrentWorld.locationAt
+            (CurrentLocation.XCoordinate - 1, CurrentLocation.YCoordinate);
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+            {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
     }
 }
--- WPFUI/MainWindow.xaml.cs
+++ WPFUI/MainWindow.xaml.cs
@@ -30,9 +30,24 @@ namespace WPFUI
             DataContext = _gameSession;
         }
 
-        private void Window_Selected(object sender, RoutedEventArgs e)
+        private void OnClick_MoveNorth(object sender, RoutedEventArgs e)
         {
+            _gameSession.MoveNorth();
+        }
 
+        private void OnClick_MoveEast(object sender, RoutedEventArgs e)
+        {
+            _gameSession.MoveEast();
+        }
+
+        private void OnClick_MoveSouth(object sender, RoutedEventArgs e)
+        {
+            _gameSession.MoveSouth();   
+        }
+
+        private void OnClick_MoveWest(object sender, RoutedEventArgs e)
+        {
+            _gameSession.MoveWest();
         }
     }
 }

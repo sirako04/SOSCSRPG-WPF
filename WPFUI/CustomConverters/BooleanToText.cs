fe92ac041ba1133f31f10fad34512af08b3ae2fa
Sirako <karissirak6@gmail.com>
2024-12-26 16:32:06 +0100

    The "done?" is now yes or no and not boolean value anymore

---
+++ WPFUI/CustomConverters/BooleanToText.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace WPFUI.CustomConverters
+{
+    public class BooleanToText : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string convertedValue = value.ToString();
+
+            if (convertedValue =="False")
+            {
+                convertedValue = "Nop";
+            }
+
+            if (convertedValue == "True")
+            {
+                convertedValue = "Yes";
+            }
+
+            return convertedValue;
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
